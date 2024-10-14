e5542cdf07c4c79f0babc55b1fcaf5d156f88f38
Jeffrey Liu <jeffreyliu689@gmail.com>
2024-10-13 22:49:10 -0400

    asdf

---
+++ app/src/main/java/com/example/deliveravble1/LessorLoginPage.java
@@ -0,0 +1,31 @@
+package com.example.deliveravble1;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LessorLoginPage extends AppCompatActivity {
+    String username = getIntent().getStringExtra("USERNAME");
+    String password = getIntent().getStringExtra("PASSWORD");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lessor_login_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    TextView uname = (TextView) findViewById(R.id.textLessorLogin);
+    uname.setText("Hello, " + username + " you are logged in as lessor.");
+}
---
+++ app/src/main/java/com/example/deliveravble1/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.deliveravble1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    String username, password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onCreateAccountButtonLessor(View view) {
+        Intent intent = new Intent(getBaseContext(), LessorLoginPage.class);
+        intent.putExtra("USERNAME", username);
+        intent.putExtra("PASSWORD", password);
+        startActivity(intent);
+    }
+
+
+}
